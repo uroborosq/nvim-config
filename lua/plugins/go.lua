a4e4f52c99c773730d9f1de3fea7758450ed037d
Dmitriy Tikhomirov <dmitriy.tikhomirov@yadro.com>
2024-05-25 19:48:36 +0300

    ogo

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,38 +1,63 @@
-return { 
-  {
-    "jay-babu/mason-null-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(
-        opts.ensure_installed,
-        { "golangci-lint", "goimports-reviser" }
-      )
-    end,
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(
-        opts.ensure_installed,
-        { "golangci-lint", "goimports-reviser" }
-      )
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        go = { "gofumpt", "goimports-reviser" },
+return {
+      {
+        "jay-babu/mason-null-ls.nvim",
+        optional = true,
+        opts = function(_, opts)
+          opts.ensure_installed = require("astrocore").list_insert_unique(
+            opts.ensure_installed,
+            { "golangci-lint", "goimports-reviser" }
+          )
+        end,
       },
-    },
-  },
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        go = { "golangcilint" }
+      {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        optional = true,
+        opts = function(_, opts)
+          opts.ensure_installed = require("astrocore").list_insert_unique(
+            opts.ensure_installed,
+            { "golangci-lint", "goimports-reviser" }
+          )
+        end,
+      },
+      {
+        "stevearc/conform.nvim",
+        opts = {
+          formatters_by_ft = {
+            go = { "gofumpt", "goimports-reviser" },
+          },
+        },
+      },
+      {
+        "mfussenegger/nvim-lint",
+        opts = {
+          linters_by_ft = {
+            go = { "golangcilint" }
+          }
+        }
       }
+    },
+    {
+      "leoluz/nvim-dap-go",
+      config = function()
+        require("dap-go").setup {
+          dap_configurations = {
+            {
+              type = "go",
+              name = "Attach remote",
+              mode = "remote",
+              request = "attach",
+              connect = {
+                host = "localhost",
+                port = "2345"
+              },
+              substitutePath = {
+                { to = '/tmp/control_path', from = '${workspaceFolder}' },
+              },
+            },
+          },
+          delve = {
+            port = "2345"
+          },
+        }
+      end
     }
-  }
-}
