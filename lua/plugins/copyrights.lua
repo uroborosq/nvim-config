28322e009bb487a0fae444667cdd6e4596183af3
Dmitriy Tikhomirov <dmitriy.tikhomirov@yadro.com>
2024-06-24 18:50:04 +0300

    yo

--- lua/plugins/copyrights.lua
+++ lua/plugins/copyrights.lua
@@ -1,13 +1,13 @@
 return {
     { "alpertuna/vim-header" },
-    {
-        "https://git.sr.ht/~reggie/licenses.nvim",
-        config = function()
-            require("licenses").setup {
-                license = function() return "LICENSE" end,
-                copyright_holder = "kek",
-            }
-            require("telescope").load_extension "licenses-nvim"
-        end,
-    },
+    -- {
+    --     "https://git.sr.ht/~reggie/licenses.nvim",
+    --     config = function()
+    --         require("licenses").setup {
+    --             license = function() return "LICENSE" end,
+    --             copyright_holder = "kek",
+    --         }
+    --         require("telescope").load_extension "licenses-nvim"
+    --     end,
+    -- },
 }
