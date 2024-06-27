2cb14699d5aea6aa0d73d3e03a7e0f07d729be1e
Dmitriy Tikhomirov <uroborosq@ya.ru>
2024-06-27 22:07:05 +0300

    add lazydev

--- lua/community.lua
+++ lua/community.lua
@@ -57,4 +57,6 @@ return {
     { import = "astrocommunity.terminal-integration.toggleterm-manager-nvim" },
     -- pure lua fzf
     { import = "astrocommunity.fuzzy-finder.fzf-lua" },
+    -- neovim development
+    { import = "astrocommunity.neovim-lua-development.lazydev-nvim" },
 }
