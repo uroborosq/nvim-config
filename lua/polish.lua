11929a694e744e4ab2d66f837bb2e2fdc54092fa
Dmitriy Tikhomirov <dmitriy.tikhomirov@yadro.com>
2024-07-14 00:20:18 +0300

    autowrite

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,6 +1,7 @@
 vim.keymap.set({ "n", "v" }, "d", '"_d', { noremap = true })
 vim.keymap.set("n", "dd", '"_dd', { noremap = true })
 vim.o.autoread = true
+vim.o.autowrite = true
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
     command = "if mode() != 'c' | checktime | endif",
     pattern = { "*" },
