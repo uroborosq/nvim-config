502590f1cd6adf9bfc56fd284da40a23b23319be
Dmitriy Tikhomirov <dmitriy.tikhomirov@yadro.com>
2024-07-25 02:08:16 +0300

    kek remappings

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,7 +1,13 @@
 vim.keymap.set({ "n", "v" }, "d", '"_d', { noremap = true })
+vim.keymap.set({ "n", "v" }, "<Del>", '"_<Del>', { noremap = true })
 vim.keymap.set("n", "dd", '"_dd', { noremap = true })
+vim.keymap.set({ "n", "v" }, "s", '"_s', { noremap = true })
+vim.keymap.set({ "n", "v" }, "p", "P", { noremap = true })
+
 vim.o.autoread = true
+
 vim.o.autowrite = true
+
 vim.diagnostic.config {
     virtual_text = false,
 }
