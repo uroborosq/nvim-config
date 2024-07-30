a66ff39e2656255779f4e9a79c86858556658554
Dmitriy Tikhomirov <uroborosq@ya.ru>
2024-07-31 02:16:10 +0300

    disable autowrite

--- lua/polish.lua
+++ lua/polish.lua
@@ -5,7 +5,6 @@ vim.keymap.set({ "n", "v" }, "s", '"_s', { noremap = true })
 vim.keymap.set({ "n", "v" }, "p", "P", { noremap = true })
 
 vim.o.autoread = true
-vim.o.autowrite = true
 
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
     command = "if mode() != 'c' | checktime | endif",
