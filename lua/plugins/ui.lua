ffcb8ab02ca67bb4cf3a701b41f0518f441eaa3e
Dmitriy Tikhomirov <uroborosq@ya.ru>
2024-08-31 00:47:23 +0300

    change theme to everforest again

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,7 +4,7 @@ return { -- selected colorscheme
         "AstroNvim/astroui",
         ---@type AstroUIOpts
         opts = {
-            colorscheme = "bamboo",
+            colorscheme = "everforest",
         },
     },
     {
