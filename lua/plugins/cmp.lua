b1dca363426122d6e1c113fee4386a94f6bf03ce
Dmitriy Tikhomirov <uroborosq@ya.ru>
2024-09-13 20:59:40 +0300

    a

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,7 +4,8 @@ return {
         opts = function(_, opts)
             local cmp = require "cmp"
             opts.sources = cmp.config.sources {
-                { name = "nvim_lsp" },
+                { name = "nvim_lsp", priority = 1000000 },
+                { name = "buffer", priority = 0 },
             }
 
             opts.sorting = {
