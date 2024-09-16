a7a4fd347513d2ffbcdcafae609001ba10c7b240
Dmitriy Tikhomirov <uroborosq@ya.ru>
2024-09-17 01:05:32 +0300

    config term size

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -2,6 +2,10 @@ return {
     "akinsho/toggleterm.nvim",
     opts = {
         direction = "float",
+        float_opts = {
+            width = function() return math.floor(vim.o.columns * 0.9) end,
+            height = function() return math.floor(vim.o.lines * 0.8) end,
+        },
         winbar = {
             enabled = true,
             name_formatter = function(term) --  term: Terminal
