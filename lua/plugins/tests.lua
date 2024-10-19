193bd1c7e901c4b7adac77ad39413ddceba29ecf
Dmitriy Tikhomirov <uroborosq@ya.ru>
2024-10-19 21:02:20 +0300

    some useless changes

--- lua/plugins/tests.lua
+++ lua/plugins/tests.lua
@@ -20,6 +20,19 @@ return {
                 },
             } -- Specify configuration
             require("neotest").setup {
+                discovery = {
+                    -- Drastically improve performance in ginormous projects by
+                    -- only AST-parsing the currently opened buffer.
+                    enabled = true,
+                    -- Number of workers to parse files concurrently.
+                    -- A value of 0 automatically assigns number based on CPU.
+                    -- Set to 1 if experiencing lag.
+                    concurrent = 0,
+                },
+                running = {
+                    -- Run tests concurrently when an adapter provides multiple commands to run.
+                    concurrent = true,
+                },
                 adapters = {
                     require "neotest-golang"(config), -- Apply configuration
                 },
