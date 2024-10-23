298bead22fd60d863c828ff66bae9e9fb11c1d1b
Dmitriy Tikhomirov <uroborosq@ya.ru>
2024-10-24 00:08:19 +0300

    cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -14,7 +14,16 @@ return {
             -- opts.mapping["<C-j>"] = cmp.mapping.select_next_item()
             -- opts.mapping["<C-k>"] = cmp.mapping.select_prev_item()
             -- opts.mapping["<C-j>"] = opts.mapping["<Tab>"]
-            -- opts.mapping["<Tab>"] = nil
+            -- opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+            -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+            -- if cmp.visible() then
+            -- local entry = cmp.get_selected_entry()
+            -- if not entry then cmp.select_next_item { behavior = cmp.SelectBehavior.Select } end
+            -- cmp.confirm()
+            -- else
+            -- fallback()
+            -- end
+            -- end, { "i", "s", "c" })
         end,
     },
     -- Tabby settings
