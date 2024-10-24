9334f09fcfd7659e5e55f64cb9c3adc913d75531
Dmitriy Tikhomirov <uroborosq@ya.ru>
2024-10-24 23:57:25 +0300

    remove CR from disabled in hardtime

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -3,7 +3,7 @@ return {
         "m4xshen/hardtime.nvim",
         dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
         opts = {
-            disabled_keys = {
+            restricted_keys = {
                 ["<CR>"] = {},
             },
         },
