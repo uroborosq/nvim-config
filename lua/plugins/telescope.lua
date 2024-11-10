2ff438d9dbe260295c586d7b795f8d6f0ab0297b
Dmitriy Tikhomirov <uroborosq@ya.ru>
2024-11-10 23:22:27 +0300

    add telescopre settings

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        opts = {
+
+            pickers = {
+                find_files = {
+                    hidden = true,
+                },
+            },
+        },
+    },
+}
