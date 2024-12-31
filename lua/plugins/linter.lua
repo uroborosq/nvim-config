58e6a8fdf3854af414f9c8d176c28e1638c73ba5
Dmitriy Tikhomirov <uroborosq@ya.ru>
2024-12-31 03:43:04 +0300

    filter out typecheck issues from golangci-lint

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -6,5 +6,19 @@ return {
                 go = { "golangcilint" },
             },
         },
+        init = function(_)
+            local lint = require "lint"
+            local result = vim.deepcopy(lint.linters.golangcilint)
+
+            lint.linters.golangcilint.parser = function(...)
+                local diagnostics = result.parser(...)
+                local filtered = {}
+                for _, d in ipairs(diagnostics) do
+                    if not (d.source == "typecheck") then table.insert(filtered, d) end
+                end
+
+                return filtered
+            end
+        end,
     },
 }
