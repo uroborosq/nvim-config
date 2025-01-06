35ea1ab1c7154650a922503f769956492769ceba
Dmitriy Tikhomirov <uroborosq@ya.ru>
2025-01-06 22:15:23 +0300

    fix theme caching

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,32 +4,7 @@ return { -- selected colorscheme
     --     opts = { options = { o = { background = "dark" } } },
     -- },
     -- installed colorschemes
-    "doums/darcula",
-    {
-        {
-            "xiantang/darcula-dark.nvim",
-            config = function()
-                -- setup must be called before loading
-                require("darcula").setup {
-                    -- override = function(_)
-                    --     return {
-                    --         background = "#333333",
-                    --         dark = "#000000",
-                    --     }
-                    -- end,
-                    opt = {
-                        integrations = {
-                            telescope = true,
-                            lualine = true,
-                            lsp_semantics_token = true,
-                            nvim_cmp = true,
-                            dap_nvim = true,
-                        },
-                    },
-                }
-            end,
-        },
-    },
+    "xiantang/darcula-dark.nvim",
     "rktjmp/lush.nvim",
     {
         "AstroNvim/astrocommunity",
