c327a0435a3aa27d18228917ba2365a89da4f59b
Dmitriy Tikhomirov <uroborosq@ya.ru>
2025-09-07 14:18:58 +0300

    add wakastat

--- lua/plugins/wakatime.lua
+++ lua/plugins/wakatime.lua
@@ -1,3 +1,25 @@
 return {
-  "wakatime/vim-wakatime",
+  { "wakatime/vim-wakatime" },
+  {
+    "fiqryq/wakastat.nvim",
+    opts = {
+      format = "Coding: %s", -- %s replaced with time
+    },
+    event = "VeryLazy",
+    cmd = { "WakastatRefresh", "WakastatStatus" },
+    specs = {
+      {
+        "rebelot/heirline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          opts.statusline = opts.statusline or {}
+          table.insert(opts.statusline, 6, { -- insert at position 5
+            provider = function() return " " .. require("wakastat").status() .. " " end,
+            hl = "Wakastat",
+            update = { "User", pattern = "WakastatUpdated" },
+          })
+        end,
+      },
+    },
+  },
 }
