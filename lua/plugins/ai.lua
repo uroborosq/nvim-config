36c02266a0be0891d266ff3249f20ae244b871b8
Dmitriy Tikhomirov <uroborosq@ya.ru>
2026-01-17 00:15:16 +0300

    disable avante

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -2,64 +2,64 @@ local openai_api_key = os.getenv "YADRO_API_KEY" -- взять из выданн
 local openai_base_endpoint = "https://litellm-proxy.ai.yadro.com"
 
 return {
-  {
-    "ravitemer/mcphub.nvim",
-    cond = not (openai_api_key == nil),
-  },
-  {
-    "yetone/avante.nvim",
-    cond = not (openai_api_key == nil),
-    opts = {
-      system_prompt = function()
-        local hub = require("mcphub").get_hub_instance()
-        return hub and hub:get_active_servers_prompt() or ""
-      end,
-      -- Using function prevents requiring mcphub before it's loaded
-      custom_tools = function()
-        return {
-          require("mcphub.extensions.avante").mcp_tool(),
-        }
-      end,
-      provider = "qwen3-coder",
-      behaviour = {
-        auto_suggestions = false,
-      },
-      providers = {
-        ["qwen3-coder"] = {
-          model = "Qwen3-Coder-30B-A3B-Instruct-FP8",
-          __inherited_from = "openai",
-          endpoint = "https://litellm-proxy.ai.yadro.com",
-          api_key_name = "YADRO_API_KEY",
-          context_window = 262144,
-          extra_request_body = {
-            max_tokens = 65336,
-          },
-        },
-        ["deepseek-r1"] = {
-          __inherited_from = "openai",
-          endpoint = "https://litellm-proxy.ai.yadro.com",
-          api_key_name = "YADRO_API_KEY",
-          context_window = 32768,
-          model = "beta/chat/deepseek-r1:70b-llama-distill-fp16",
-        },
-        ["qwen2.5-coder:7b"] = {
-          model = "autocomplete/qwen2.5-coder:7b",
-          endpoint = "https://litellm-proxy.ai.yadro.com",
-          api_key_name = "YADRO_API_KEY",
-          __inherited_from = "openai",
-          context_window = 20000,
-          timeout = 1,
-          extra_request_body = {
-            max_tokens = 100,
-          },
-          ["nomic-embed-text"] = {
-            __inherited_from = "openai",
-            model = "embed/nomic-embed-text",
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "ravitemer/mcphub.nvim",
+  --   cond = not (openai_api_key == nil),
+  -- },
+  -- {
+  --   "yetone/avante.nvim",
+  --   cond = not (openai_api_key == nil),
+  --   opts = {
+  --     system_prompt = function()
+  --       local hub = require("mcphub").get_hub_instance()
+  --       return hub and hub:get_active_servers_prompt() or ""
+  --     end,
+  --     -- Using function prevents requiring mcphub before it's loaded
+  --     custom_tools = function()
+  --       return {
+  --         require("mcphub.extensions.avante").mcp_tool(),
+  --       }
+  --     end,
+  --     provider = "qwen3-coder",
+  --     behaviour = {
+  --       auto_suggestions = false,
+  --     },
+  --     providers = {
+  --       ["qwen3-coder"] = {
+  --         model = "Qwen3-Coder-30B-A3B-Instruct-FP8",
+  --         __inherited_from = "openai",
+  --         endpoint = "https://litellm-proxy.ai.yadro.com",
+  --         api_key_name = "YADRO_API_KEY",
+  --         context_window = 262144,
+  --         extra_request_body = {
+  --           max_tokens = 65336,
+  --         },
+  --       },
+  --       ["deepseek-r1"] = {
+  --         __inherited_from = "openai",
+  --         endpoint = "https://litellm-proxy.ai.yadro.com",
+  --         api_key_name = "YADRO_API_KEY",
+  --         context_window = 32768,
+  --         model = "beta/chat/deepseek-r1:70b-llama-distill-fp16",
+  --       },
+  --       ["qwen2.5-coder:7b"] = {
+  --         model = "autocomplete/qwen2.5-coder:7b",
+  --         endpoint = "https://litellm-proxy.ai.yadro.com",
+  --         api_key_name = "YADRO_API_KEY",
+  --         __inherited_from = "openai",
+  --         context_window = 20000,
+  --         timeout = 1,
+  --         extra_request_body = {
+  --           max_tokens = 100,
+  --         },
+  --         ["nomic-embed-text"] = {
+  --           __inherited_from = "openai",
+  --           model = "embed/nomic-embed-text",
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
   {
     "milanglacier/minuet-ai.nvim",
     cond = not (openai_api_key == nil),
