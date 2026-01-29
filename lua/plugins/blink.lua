d17a0e7eba6daeaed28e29c6543cccb16035d988
Dmitriy Tikhomirov <uroborosq@ya.ru>
2026-01-29 22:20:06 +0300

    remove buggy suggesters

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,12 +3,6 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   dependencies = {
-    {
-      "edte/blink-go-import.nvim",
-      ft = "go",
-      config = function() require("blink-go-import").setup() end,
-    },
-    { "samiulsami/cmp-go-deep", dependencies = { "kkharji/sqlite.lua" } },
     { "saghen/blink.compat" },
   },
   opts = {
@@ -34,44 +28,13 @@ return {
     },
 
     keymap = {
-      -- ["<Tab>"] = {
-      --   function(cmp)
-      --     if cmp.snippet_active() then
-      --       return cmp.accept()
-      --     else
-      --       return cmp.select_and_accept()
-      --     end
-      --   end,
-      --   "snippet_forward",
-      --   "fallback",
-      -- },
-      -- ["<S-Tab>"] = { "snippet_backward", "fallback" },
-      -- ["<Tab>"] = {},
       ["<C-N>"] = {},
       ["<C-P>"] = {},
       ["<C-Space>"] = { function(cmp) cmp.show { providers = { "snippets" } } end },
     },
     sources = {
-      default = {--[[  "go_deep", "go_pkgs", ]]
-        "lsp",
-        "path",
-      },
+      default = { "lsp", "path" },
       providers = {
-        go_pkgs = {
-          enabled = false,
-          module = "blink-go-import",
-          name = "Import",
-        },
-        go_deep = {
-          enabled = false,
-          name = "go_deep",
-          module = "blink.compat.source",
-          min_keyword_length = 3,
-          max_items = 5,
-          ---@module "cmp_go_deep"
-          ---@type cmp_go_deep.Options
-          opts = {},
-        },
         snippets = {
           enabled = false,
         },
