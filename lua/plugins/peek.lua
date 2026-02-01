ea8ad0c5590713e2067a611458799e4469bdb97f
Dmitriy Tikhomirov <uroborosq@ya.ru>
2026-02-01 23:09:27 +0300

    d

--- lua/plugins/peek.lua
+++ lua/plugins/peek.lua
@@ -1,10 +1,26 @@
 return {
-  "toppair/peek.nvim",
-  event = { "VeryLazy" },
-  build = "deno task --quiet build:fast",
-  config = function()
-    require("peek").setup()
-    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-  end,
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup {
+        -- app = "browser",
+      }
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          ["<Leader>mo"] = { ":PeekOpen<CR>", desc = "Open markdown webview" },
+          ["<Leader>mc"] = { ":PeekClose<CR>", desc = "Close markdown webview" },
+        },
+      },
+    },
+  },
 }
