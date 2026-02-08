80a23c7616e6d43e21ecb37b2ff92ffd69bd1d10
Dmitriy Tikhomirov <uroborosq@ya.ru>
2026-02-08 23:51:28 +0300

    df

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "stevearc/aerial.nvim",
+    opts = {
+      layout = {
+        default_direction = "prefer_left",
+        max_width = nil,
+        width = nil,
+        min_width = nil,
+      },
+    },
+  },
+}
