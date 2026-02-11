421ee47dd82f4fe25d3825dd913fd0fd0548315b
Dmitriy Tikhomirov <dmitriy.tikhomirov@yadro.com>
2026-02-11 10:26:48 +0300

    clean previous config

--- lua/plugins/zen.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "folke/snacks.nvim",
-  opts = function(_, opts)
-    opts.zen.toggles.diagnostics = true
-    opts.zen.toggles.inlay_hints = false
-    opts.zen.win.wo.relativenumber = true
-  end,
-}
