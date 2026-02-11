21c42ec68db4474b6dc5b6d66a7f038f9802d859
Dmitriy Tikhomirov <uroborosq@ya.ru>
2026-02-11 10:20:18 +0300

    specify preview left/right mappings telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,17 @@
+local actions = require "telescope.actions"
+
 return {
   {
     "nvim-telescope/telescope.nvim",
     opts = {
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-f>"] = actions.preview_scrolling_left,
+            ["<C-b>"] = actions.preview_scrolling_right,
+          },
+        },
+      },
       extensions = {
         ["ui-select"] = {},
       },
