7fe183d5fd3d15c6e3204fd6647e03f155ded809
Dmitriy Tikhomirov <dmitriy.tikhomirov@yadro.com>
2026-02-15 11:08:45 +0300

    ccommit

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,8 @@
+return {
+	{
+		"numToStr/Comment.nvim",
+		keys = {
+			{ "<Leader>/", "gc", mode = { "n", "v", "x" }, desc = "Toggle comment" },
+		},
+	},
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,10 @@ local lang_prefix = "<Leader>l"
 
 return {
 	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		opts = {},
+	},
+	{
 
 		"nvim-telescope/telescope.nvim",
 		version = "*",
@@ -12,7 +16,7 @@ return {
 			"nvim-lua/plenary.nvim",
 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		},
-		config = function(_, _)
+		config = function(_, opts)
 			local builtin = require("telescope.builtin")
 			local action = require("telescope.actions")
 
@@ -26,31 +30,39 @@ return {
 				{ search_prefix .. "t", builtin.colorscheme, desc = "Farbschema" },
 				{ search_prefix .. "n", ":Telescope notify<cr>", desc = "Benachrichtigungen" },
 				{ lang_prefix .. "s", ":Telescope aerial<cr>", desc = "LSP Symbolen suchen" },
+				{ lang_prefix .. "R", builtin.lsp_references, desc = "LSP Symbolen suchen" },
+				{ lang_prefix .. "D", builtin.diagnostics, desc = "LSP Probleme sehen" },
+				{ lang_prefix .. "R", builtin.lsp_references, desc = "LSP References suchen" },
+				{ "gy", builtin.lsp_type_definitions, desc = "LSP definitions suchen" },
+				{ lang_prefix .. "i", builtin.lsp_implementations, desc = "LSP Implementations suchen" },
+				{ lang_prefix .. "G", builtin.lsp_dynamic_workspace_symbols, desc = "LSP Workspace Symbolen suchen" },
+				{ lang_prefix .. "r", vim.lsp.buf.rename, desc = "LSP symbol rename" },
+				{ "gd", builtin.lsp_definitions, desc = "zu Definition gehen" },
+				{ lang_prefix .. "I", vim.lsp.buf.typehierarchy, desc = "LSP symbol ierarcchy" },
 			})
 
 			require("telescope").setup({
-				{
-					defaults = {
-						mappings = {
-							i = {
-								["<C-j>"] = action.move_selection_next,
-								["<C-k>"] = action.move_selection_previous,
-								["<C-f>"] = action.preview_scrolling_left,
-								["<C-b>"] = action.preview_scrolling_right,
-							},
-						},
-					},
-					extensions = {
-						["ui-select"] = {},
-					},
-					pickers = {
-						find_files = {
-							hidden = true,
+				defaults = {
+					mappings = {
+						i = {
+							["<C-j>"] = action.move_selection_next,
+							["<C-k>"] = action.move_selection_previous,
+							["<C-f>"] = action.preview_scrolling_left,
+							["<C-b>"] = action.preview_scrolling_right,
 						},
 					},
 				},
+				extensions = {
+					["ui-select"] = {},
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
 			})
-            require("telescope").load_extension("aerial")
+			require("telescope").load_extension("aerial")
+			require("telescope").load_extension("ui-select")
 		end,
 	},
 }
