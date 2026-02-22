ce8875bd04d3f54cae750d058afd942fc72b9389
Dmitriy Tikhomirov <uroborosq@ya.ru>
2026-02-22 22:14:35 +0300

    explicit enable input

--- lua/plugins/input.lua
+++ lua/plugins/input.lua
@@ -3,10 +3,6 @@ return {
 	"folke/snacks.nvim",
 	---@type snacks.Config
 	opts = {
-		input = {
-			-- your input configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
+		input = { enabled = true },
 	},
 }
