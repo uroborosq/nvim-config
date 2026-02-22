7db1878da46d9d287611fb0438a116441efb3763
Dmitriy Tikhomirov <uroborosq@ya.ru>
2026-02-22 13:36:19 +0300

    ne

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -1,5 +1,34 @@
-vim.o.guifont = "Adwaita Mono:h11:#e-subpixelantialias" -- text below applies for VimScript
-vim.opt.linespace = 3
--- vim.g.neovide_position_animation_length = 0.03
+if vim.g.neovide then
+	local font = "Adwaita Mono:#e-subpixelantialias"
+	local size = 11
+	vim.o.guifont = font .. ":h" .. tostring(size)
+
+	-- vim.g.neovide_cursor_vfx_mode = "railgun"
+	-- vim.g.neovide_cursor_vfx_particle_density = 2
+	-- vim.g.neovide_position_animation_length = 0.10
+	vim.g.neovide_cursor_animation_length = 0.10
+	vim.g.neovide_cursor_short_animation_length = 0.03
+	vim.g.neovide_cursor_trail_size = 0.05
+	vim.g.neovide_cursor_antialiasing = false
+	vim.g.neovide_progress_bar_enabled = true
+	vim.g.neovide_progress_bar_height = 5.0
+	vim.g.neovide_progress_bar_animation_speed = 200.0
+	vim.g.neovide_progress_bar_hide_delay = 0.2
+
+	vim.g.neovide_hide_mouse_when_typing = true
+
+	vim.keymap.set("n", "<C-->", function()
+		size = size - 1
+		vim.o.guifont = font .. ":h" .. tostring(size)
+	end)
+
+	-- увеличить
+	vim.keymap.set("n", "<C-=>", function()
+		size = size + 1
+		vim.o.guifont = font .. ":h" .. tostring(size)
+	end)
+
+	vim.opt.linespace = 3
+end
 
 return {}
