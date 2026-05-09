9c52ab27a704824d3b1b9e4b5b7b11f1eb002933
Dmitriy Tikhomirov <uroborosq@ya.ru>
2026-05-09 19:49:17 +0300

    sehr gut

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -65,8 +65,8 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "]T", "<cmd>tabnext<cr>", { desc = "das Nachtest Tab", silent = true })
-vim.keymap.set("n", "[T", "<cmd>tabprev<cr>", { desc = "ver", silent = true })
+vim.keymap.set({ "n", "t" }, "]T", "<cmd>tabnext<cr>", { desc = "das Nachtest Tab", silent = true })
+vim.keymap.set({ "n", "t" }, "[T", "<cmd>tabprev<cr>", { desc = "ver", silent = true })
 
 vim.opt.tabstop = 4
 vim.opt.termguicolors = false
