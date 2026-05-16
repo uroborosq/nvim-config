044f4e9d69e2cebc2e96c7ccb7165339443cd078
Dmitriy Tikhomirov <uroborosq@ya.ru>
2026-05-16 20:44:46 +0300

    notify

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,6 +1,9 @@
 return {
 	{
 		"rcarriga/nvim-notify",
+		opts = {
+			background_colour = "#00000000",
+		},
 		config = function(_, opts)
 			require("notify").setup(opts)
 			vim.notify = require("notify")
